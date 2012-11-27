23009107a19a793d240bdcc233d53b49bb563fee
Harssh <harssh@headfitted.com>
2012-11-27 09:14:45 +0530

    Make a basic model (including secure passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
-  
+  has_secure_password    
   before_save { |user| user.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -23,5 +23,5 @@ class User < ActiveRecord::Base
                    
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence:true
-  has_secure_password                  
+                
 end
