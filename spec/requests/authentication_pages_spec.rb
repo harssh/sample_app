d4e526a7b4c42852a5cb7d056e35c5f5fc64d421
Harssh <harssh@headfitted.com>
2012-11-30 10:54:48 +0530

    Finish user edit,update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,7 +10,7 @@ describe "Authentication" do
     it { should have_selector('h1', text: 'Sign in')}
     
     it { should have_selector('title', text: 'Sign in')}
-  end
+  end      # signin page end
   
   
       describe "signin" do
@@ -27,9 +27,9 @@ describe "Authentication" do
                   
                   before { click_link "Home" }
                   it { should_not have_selector('div.alert.alert-error')}
-                end
+                end  # visiting end
 
-           end
+           end # invalid info end
            
            describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -43,13 +43,85 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-           
+      it { should have_link('Settings', href: edit_user_path(user)) }     
               describe "followed by signout" do
             
                 before { click_link "Sign out"}
                 it { should have_link('Sign in') }
-                end
-           end
-      end
+              end  # followed by signout end
+           end  # with valid info end
+      end  # signin end
   
-  end
+  
+   
+        describe "authorization" do
+          
+    
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+      
+      before { sign_in non_admin }
+      
+      describe "submitting a DEELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+        
+      end
+       
+    end
+    
+          describe "for non-signed-in users" do
+            
+            
+            let(:user) { FactoryGirl.create(:user) }
+            
+        
+                  describe " when attempting  to visit a protected page" do
+                    
+                    before do
+                      visit edit_user_path(user)
+                      fill_in "Email", with: user.email
+                      fill_in "Password", with: user.password
+                      
+                      click_button "Sign in"
+                      
+                    end
+                     
+                     
+                    describe "after signing in" do
+                      
+                      it "should render the desired protected page" do
+                        page.should have_selector('title', text: 'Edit user')
+                      end
+                      
+                      
+                    end
+                  end # end of when attempting to visit a protected page
+                                   
+                     describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_url) }
+        end
+
+        describe "visiting user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+       
+
+        
+      end                
+             end # end of non signed in users
+          
+        end  # end of "authorization" do
+  
+end  # authentication end
